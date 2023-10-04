0b5b6fc81050a10780b7f2d215beb3be732ae560
“terrytao” <color1252002@gmail.com>
2023-10-04 17:44:08 +0800

    last read ver

--- read3_sum.py
+++ read3_sum.py
@@ -7,7 +7,7 @@ data =[]
 count = 0
 with open ('reviews.txt', 'r') as a :
 	for review in a :
-		print(review) 
+		#print(review) 
 		data.append(review.strip())
 		#data.append(review)
 		count += 1
@@ -23,3 +23,27 @@ for d in data :
 print('總平均長度', sum_len/len(data))	
 
 
+new=[]
+
+for d in data :
+	if len(d) < 100 :
+		new.append(d)
+print('總共有', len(new), '筆資料小於長度100')
+
+
+good = []
+for s in data :
+	if 'good' in s : #有多少good str in s 這虛擬清單 # ex: print('a' in 'abc's) = true
+		good.append(s)
+
+print('總共有', len(good), '筆資料擁有good的字串')
+print('a' in 'abc')
+
+#class 58 list 快寫法 
+
+#line 34~40
+
+good = [ s for s in data if 'good' in d ] # == line 34~37 # s 也可取代其他東西(1/true/sth)
+bad = ['bad' in s for s in data ] # 確認bad 有沒有在 s 裡（從data 掉出的s 放入good清單）true/false
+
+print(bad)
