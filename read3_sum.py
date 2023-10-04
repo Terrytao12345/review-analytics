463286856b7fbd479cdf7dd0b1fd732d54f1d94c
“terrytao” <color1252002@gmail.com>
2023-10-04 15:51:30 +0800

    read + for loop + average

---
+++ read3_sum.py
@@ -0,0 +1,22 @@
+#review analytics
+
+data =[]
+count = 0
+with open ('reviews.txt', 'r') as a :
+	for review in a :
+		print(review) 
+		data.append(review.strip())
+		#data.append(review)
+		count += 1
+		if count % 1000 == 0 : # ％>後面於數多少 == ?? ,確認除1000後 於數為0的
+			print(len(data))
+
+print('檔案讀取完了,總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data :
+	sum_len = sum_len + len(d) 
+
+print('總平均長度', sum_len/len(data))	
+
+
